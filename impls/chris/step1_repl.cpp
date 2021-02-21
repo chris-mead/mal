1c3922f85f7ddd0688b25e5d5806837d5cc92f89
Chris Mead <chris@chris.com>
2021-02-21 16:16:20 +0000

    Remove +C++ 20 dependency and clean up step1 read/eval loop

--- impls/chris/step1_repl.cpp
+++ impls/chris/step1_repl.cpp
@@ -1,6 +1,33 @@
+#include <iostream>
+#include <string>
 
+const std::string_view DEFAULT_PROMPT {"user> "};
+
+class ConfigInfo
+{
+public:
+    std::string_view prompt {DEFAULT_PROMPT};
+};
+
+int mainLoop(const ConfigInfo& config_info)
+{
+    bool done = false;
+    std::string line;
+    while(!done)
+    {
+        std::cout << config_info.prompt;
+        std::getline(std::cin, line);
+        if(std::cin.eof())
+            break;
+
+        std::cout << "\n"
+                  << line << "\n";
+    }
+    return 0;
+}
 
 int main()
 {
-    return 0;
-}
+    ConfigInfo config_info;
+    const auto result = mainLoop(config_info);
+    return result;
