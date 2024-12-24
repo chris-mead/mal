bbd8e63db300818b03173ac2bb3b4c4cdbd85f03
Chris Mead <45538085+chris-mead@users.noreply.github.com>
2024-12-24 17:13:38 +0000

    Remove +C++ 20 dependency and clean up step1 read/eval loop

--- impls/chris/step0_repl.cpp
+++ impls/chris/step0_repl.cpp
@@ -1,32 +1,30 @@
-#include <compare>
 #include <iostream>
 #include <string>
 
-const std::string_view DEFAULT_PROMPT{"user> "};
+const std::string_view DEFAULT_PROMPT {"user> "};
 
 class ConfigInfo
 {
 public:
-    std::string_view prompt{DEFAULT_PROMPT};
+    std::string_view prompt {DEFAULT_PROMPT};
 };
-
 
 int mainLoop(const ConfigInfo& config_info)
 {
     bool done = false;
     std::string line;
-    while (!done)
+    while(!done)
     {
         std::cout << config_info.prompt;
         std::getline(std::cin, line);
-        if (std::cin.eof())
+        if(std::cin.eof())
             break;
-        
-        std::cout << "\n" << line << "\n";
+
+        std::cout << "\n"
+                  << line << "\n";
     }
     return 0;
 }
-
 
 int main()
 {
--- impls/chris/step1_read_print.cpp
+++ impls/chris/step1_read_print.cpp
@@ -1,6 +1,72 @@
+#include <iostream>
+#include <string>
 
+const std::string_view DEFAULT_PROMPT {"user> "};
+
+class ConfigInfo
+{
+public:
+    std::string_view prompt {DEFAULT_PROMPT};
+};
+
+class LexerState
+{
+};
+
+class InterpreterState
+{
+private:
+    std::istream* in;
+    std::string prompt;
+
+public:
+    InterpreterState(const ConfigInfo& config_info_,
+                     std::istream* in_) :
+        in {in_},
+        prompt {config_info_.prompt}
+    {
+    }
+
+    std::string readLine()
+    {
+        std::string line;
+        std::getline(*in, line);
+        return line;
+    }
+
+    bool moreInput() const
+    {
+        return !in->eof();
+    }
+
+    void printPrompt()
+    {
+        std::cout << prompt;
+    }
+
+    void printVal(std::string val)
+    {
+        std::cout << "\n"
+                  << val << "\n";
+    }
+};
+
+int mainLoop(const ConfigInfo& config_info)
+{
+    InterpreterState state {config_info, &std::cin};
+    while(state.moreInput())
+    {
+        state.printPrompt();
+        std::string line = state.readLine();
+
+        state.printVal(line);
+    }
+    return 0;
+}
 
 int main()
 {
-    return 0;
+    ConfigInfo config_info;
+    const auto result = mainLoop(config_info);
+    return result;
 }
