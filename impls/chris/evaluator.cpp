475fe71793ece7551cbc50b4f6a780c66070407c
Chris Mead <45538085+chris-mead@users.noreply.github.com>
2024-12-24 17:13:38 +0000

    Add simple Environment and move builtin arithmetic funcs to use it

--- impls/chris/evaluator.cpp
+++ impls/chris/evaluator.cpp
@@ -1,2 +1,3 @@
 #include "evaluator.h"
 
+
--- impls/chris/evaluator.h
+++ impls/chris/evaluator.h
@@ -2,89 +2,106 @@
 #define EVALUATOR_H
 
 #include "lexer.h"
+#include "env.h"
 #include "result.h"
 #include "parser.h"
 
+#include <functional>
 #include <string>
 
 using EvalResult = Result<TreeNode>;
 
 class REPLEnv
 {
+    Environment root_env{};
+
+    Environment* leaf_env = &root_env;
+    
 public:
+    REPLEnv();
     EvalResult apply(std::string symbol, const std::vector<TreeNode> nodes) const;
 };
 
+inline REPLEnv::REPLEnv()
+{
+    root_env.set("+", [](auto& nodes)
+                      {
+                          int acc = 0;
+                          for(const auto& node : nodes)
+                          {
+                              auto num = std::atoi(node.token.text.c_str());
+                              acc += num;
+                          }
+                          return TreeNode(NodeKind::ATOM, Token {TokenKind::NUMBER, std::to_string(acc), 0});
+                      });
+    root_env.set("-", [](auto& nodes)
+                      {
+                          int acc;
+                          if(nodes.empty())
+                          {
+                              acc = 0;
+                          }
+                          else
+                          {
+                              auto iter = std::begin(nodes);
+                              acc = std::atoi(iter->token.text.c_str());
+                              ++iter;
+                              while(iter != std::end(nodes))
+                              {
+                                  acc -= std::atoi(iter->token.text.c_str());
+                                  ++iter;
+                              }
+                          }
+                          return TreeNode(NodeKind::ATOM, Token {TokenKind::NUMBER, std::to_string(acc), 0});
+                      });
+    
+    root_env.set("*", [](auto& nodes)
+                      {
+                          int acc = 1;
+                          for(const auto& node : nodes)
+                          {
+                              auto num = std::atoi(node.token.text.c_str());
+                              acc *= num;
+                          }
+                          return TreeNode(NodeKind::ATOM, Token {TokenKind::NUMBER, std::to_string(acc), 0});
+                      });
+
+    root_env.set("/", [](auto& nodes)
+                    {
+                        int acc;
+                        if(nodes.size() < 2)
+                        {
+                            acc = 0;
+                        }
+                        else
+                        {
+                            auto iter = std::begin(nodes);
+                            acc = std::atoi(iter->token.text.c_str());
+                            ++iter;
+                            while(iter != std::end(nodes))
+                            {
+                                int denom = std::atoi(iter->token.text.c_str());
+                                // TODO - Divide by zero
+                                // if(denom == 0)
+                                //     return EvalResult{"Divide by 0", Token{TokenKind::NUMBER, "0", 0}};
+
+                                acc /= denom;
+                                ++iter;
+                            }
+                        }
+                        return TreeNode(NodeKind::ATOM, Token {TokenKind::NUMBER, std::to_string(acc), 0});
+                    });
+        
+    
+}
+
 EvalResult inline REPLEnv::apply(std::string symbol, const std::vector<TreeNode> nodes) const
 {
-    if(symbol == "+")
-    {
-        int acc = 0;
-        for(const auto& node : nodes)
-        {
-            auto num = std::atoi(node.token.text.c_str());
-            acc += num;
-        }
-        return TreeNode(NodeKind::ATOM, Token {TokenKind::NUMBER, std::to_string(acc), 0});
-    }
-    else if(symbol == "-")
-    {
-        int acc;
-        if(nodes.empty())
-        {
-            acc = 0;
-        }
-        else
-        {
-            auto iter = std::begin(nodes);
-            acc = std::atoi(iter->token.text.c_str());
-            ++iter;
-            while(iter != std::end(nodes))
-            {
-                acc -= std::atoi(iter->token.text.c_str());
-                ++iter;
-            }
-        }
-        return TreeNode(NodeKind::ATOM, Token {TokenKind::NUMBER, std::to_string(acc), 0});
-    }
-    else if(symbol == "*")
-    {
-        int acc = 1;
-        for(const auto& node : nodes)
-        {
-            auto num = std::atoi(node.token.text.c_str());
-            acc *= num;
-        }
-        return TreeNode(NodeKind::ATOM, Token {TokenKind::NUMBER, std::to_string(acc), 0});
-    }
-    else if(symbol == "/")
-    {
-        int acc;
-        if(nodes.size() < 2)
-        {
-            acc = 0;
-        }
-        else
-        {
-            auto iter = std::begin(nodes);
-            acc = std::atoi(iter->token.text.c_str());
-            ++iter;
-            while(iter != std::end(nodes))
-            {
-                int denom = std::atoi(iter->token.text.c_str());
-                if(denom == 0)
-                    return {"Divide by 0", iter->token};
-
-                acc /= denom;
-                ++iter;
-            }
-        }
-        return TreeNode(NodeKind::ATOM, Token {TokenKind::NUMBER, std::to_string(acc), 0});
-    }
-    else
-    {
-        return TreeNode(NodeKind::ATOM, Token {TokenKind::NUMBER, "0", 0});
-    }
+    const auto* func = leaf_env->get(symbol);
+    if (func)
+        return (*func)(nodes);
+    std::string error_message = "ERROR: Could not find \"" + symbol + "\"";
+    return EvalResult(error_message, Token {TokenKind::NUMBER, "0", 0});
 }
 
 EvalResult inline evalAST(const TreeNode& node, const REPLEnv& env)
